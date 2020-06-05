c51380e308cca5073848eb8d2148f51ac9a606d8
RamachandiranVijayalakshmi <66218468+RamachandiranVijayalakshmi@users.noreply.github.com>
2020-06-05 23:22:36 +0530

    Add files via upload

---
+++ count vowel,consident,white space,words.py
@@ -0,0 +1,19 @@
+#count the number of vowels & considents,words,white spaces in a line of txt.
+a=str(input('Enter your line of string:'))#dont enter the special charactrs and numbers
+b=a.lower()
+length=len(b)
+print('length of your line',length)
+s=b.split()
+space=b.count(' ')
+c=0
+e=0
+d=["a","e","i","o","u"]
+for i in b:
+    if i in d:
+        c=c+1
+    else:               
+        e=e+1
+print("Number of vowels in given sentence is:",c)  
+print('Number of considents in given sentence is:',(e-space))    
+print('Number of whitespace in given sentence is:',space)
+print('Number of words in given sentence is:',len(s))
